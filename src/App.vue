755e6591000fc5eb6856084fad1a5214c81c7ce1
lisa <lisalisa12332180@gmail.com>
2024-06-09 16:10:18 +0800

    feat: create Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <Header></Header>
+    <router-view />
   </div>
 </template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  data() {
+    return {
+      msgTest: "Welcome to Your Vue.js App Lisa is Testing dddd",
+    };
+  },
+  components: {
+    HelloWorld,
+    Header,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +29,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="nav">
+    <div class="title">Lisa's Blog</div>
+    <div class="header-right">
+      <div class="nav-item">
+        <div>Home</div>
+        <div>About</div>
+      </div>
+      <form role="search">
+        <input type="search" placeholder="Search" aria-label="Search" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.title {
+  color: white;
+  font-size: 1.5rem;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #404040;
+}
+
+.header-right {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.nav-item {
+  display: flex;
+  align-items: center;
+  color: aliceblue;
+  cursor: pointer;
+  gap: 20px;
+}
+
+.header-right form {
+  display: flex;
+  gap: 20px;
+}
+
+.nav input {
+  border-radius: 0.25rem;
+}
+
+.nav button {
+  border-radius: 0.25rem;
+  background-color: #505050;
+  color: white;
+  border: none;
+}
+</style>
