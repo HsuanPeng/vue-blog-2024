2cda02c12b0132cdac62b33218b63d2ca993a310
lisa <lisalisa12332180@gmail.com>
2024-06-09 16:44:50 +0800

    feat: creat HomeView and get fake API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,78 @@
 <template>
-  <div class="home">HomeView</div>
+  <div class="home">
+    <section class="home-articles">
+      <article v-for="article in articles">
+        <h3>{{ article.title | subTitle }}</h3>
+        <i>{{ article.body }}</i>
+      </article>
+    </section>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HomeView",
+  mounted() {
+    const api = "https://jsonplaceholder.typicode.com/posts";
+    axios.get(api).then((response) => {
+      this.articles = response.data;
+    });
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  filters: {
+    subTitle(value) {
+      return value.substring(0, 20);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+
+  .home-articles {
+    article {
+      display: flex;
+      padding-left: 2rem;
+      justify-content: center;
+      margin: 1rem;
+      flex-direction: column;
+      align-items: flex-start;
+      width: 75vw;
+      height: 200px;
+      background-color: #dddddd;
+      border-radius: 16px;
+
+      h3 {
+        cursor: pointer;
+
+        &:hover {
+          opacity: 0.6;
+        }
+      }
+
+      i {
+        color: #8f8f8f;
+      }
+
+      span {
+        text-align: left;
+        padding-right: 1.5rem;
+      }
+    }
+  }
+
+  .home-side-bar {
+    width: 25vw;
+    padding: 1rem;
+    height: calc(100vh - 56px);
+  }
+}
+</style>
